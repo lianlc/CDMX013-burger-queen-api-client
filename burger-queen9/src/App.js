926aeee729456159d335e5656f63226435d8d7a0
Janeth Pérez <janeth_2124@hotmail.com>
2022-12-01 13:42:56 -0600

    cambios

--- burger-queen9/src/App.js
+++ burger-queen9/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-//import { RouterProvider } from 'react-router-dom';
-//import { useState } from 'react';
-//import { RouteAuth } from './routes/authRoutes/Auth';
-//import { NoAuthR } from './routes/noAuthRoutes/noAuth';
+import { RouterProvider } from 'react-router-dom';
+import { useState } from 'react';
+import { RouteAuth } from './routes/authRoutes/Auth';
+import { NoAuthR } from './routes/noAuthRoutes/noAuth';
 import{Users} from './User'
 
 function App() {
